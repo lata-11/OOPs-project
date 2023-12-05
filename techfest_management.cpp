4f92b442ee39d490389abb8519899299ac00f3b2
lata <imlata1111@gmail.com>
2023-12-05 17:20:02 +0530

    Student class updated

--- techfest_management.cpp
+++ techfest_management.cpp
@@ -2,33 +2,44 @@
 using namespace std;
 
 /* Student class */
-class Student {
+class Student
+{
 protected:
     int id;
     string name;
+    string email;
 
 public:
-    Student(int _id, const std::string& _name) : id(_id), name(_name) {}
+    Student(int _id, const string &_name, const string &_email) : id(_id), name(_name), email(_email) {}
 
-    int getId() const {
+    int getId() const
+    {
         return id;
     }
 
-    const string& getName() const {
+    const string &getName() const
+    {
         return name;
+    }
+
+    const string &getEmail() const
+    {
+        return email;
     }
 };
 
-int main() {
-    
+int main()
+{
+
     /* Student class illustration */
-    Student *student1 = new Student(1, "Lata");
+    Student *student1 = new Student(1, "Lata", "imlata1111@gmail.com");
 
     // Print student information
     cout << "Student ID: " << student1->getId() << endl;
     cout << "Student Name: " << student1->getName() << endl;
+    cout << "Student Email: " << student1->getEmail() << endl;
 
     delete student1;
-    
+
     return 0;
 }
