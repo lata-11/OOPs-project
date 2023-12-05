05dff82b4d62cfebe15df51102b300f22763493d
lata <imlata1111@gmail.com>
2023-12-05 17:36:37 +0530

    Core Member class done

--- techfest_management.cpp
+++ techfest_management.cpp
@@ -28,6 +28,60 @@ public:
     }
 };
 
+/* Coordinator class declaration */
+class Coordinator;
+
+/* Core member class */
+class CoreMember : protected Student
+{
+private:
+    int id;
+    string name;
+    string specialization;
+    vector<Coordinator *> coordinators;
+
+public:
+    CoreMember(int _id, const string &_name, const string &_specialization)
+        : Student(_id, _name, ""), id(_id), name(_name), specialization(_specialization) {}
+
+    int getId() const
+    {
+        return id;
+    }
+
+    const string &getName() const
+    {
+        return name;
+    }
+
+    const string &getSpecialization() const
+    {
+        return specialization;
+    }
+
+    const vector<Coordinator *> &getCoordinators() const
+    {
+        return coordinators;
+    }
+
+    void addCoordinator(Coordinator *coordinator)
+    {
+        coordinators.push_back(coordinator);
+    }
+
+    void removeCoordinator(Coordinator *coordinator)
+    {
+        for (auto it = coordinators.begin(); it != coordinators.end(); ++it)
+        {
+            if (*it == coordinator)
+            {
+                coordinators.erase(it);
+                break;
+            }
+        }
+    }
+};
+
 int main()
 {
 
@@ -40,6 +94,23 @@ int main()
     cout << "Student Email: " << student1->getEmail() << endl;
 
     delete student1;
+
+    /* Core member class illustration */
+
+    CoreMember coreMember(1, "John Doe", "Programming");
+
+    /* This will work after implementation of Coordinator class */
+    /*Coordinator coordinator1;
+    Coordinator coordinator2;
+
+    coreMember.addCoordinator(&coordinator1);
+    coreMember.addCoordinator(&coordinator2);
+
+    cout << "Coordinators before removal: " << coreMember.getCoordinators().size() << std::endl;
+
+    coreMember.removeCoordinator(&coordinator1);
+
+    cout << "Coordinators after removal: " << coreMember.getCoordinators().size() << std::endl;*/
 
     return 0;
 }
